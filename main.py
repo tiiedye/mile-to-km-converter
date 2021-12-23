7cda374e901ba0029e673bee0d0b4ce9813e8dd3
tiiedye <tiiedye@gmail.com>
2021-12-23 12:41:39 -0800

    getting all GUI elements together and creating calculate function

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 # Mile to KM converter
+from tkinter import *
+
+
+def calculate():
+    miles = int(user_input.get())
+    km = miles * 1.609
+    result_label["text"] = str(km)
+
+
+screen = Tk()
+screen.title("Miles to Km")
+screen.minsize(width=250, height=100)
+
+user_input = Entry()
+mile_label = Label(text="Miles")
+is_equal_to_label = Label(text="Is equal to")
+result_label = Label(text="0")
+km_label = Label(text="Km")
+calculate_button = Button(text="Calculate", command=calculate)
+
+user_input.grid(column=1, row=0)
+mile_label.grid(column=2, row=0)
+is_equal_to_label.grid(column=0, row=1)
+result_label.grid(column=1, row=1)
+km_label.grid(column=2, row=1)
+calculate_button.grid(column=1, row=2)
+
+# All code before exit
+screen.mainloop()
